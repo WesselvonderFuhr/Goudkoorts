54730142d7bad6c2bb61d6fcc9356983f85a5d30
Wessel <wjfuhr@avans.nl>
2018-10-15 13:52:33 +0200

    asdfg

--- ConsoleApp3/InputView.cs
+++ ConsoleApp3/InputView.cs
@@ -8,5 +8,24 @@ namespace ConsoleApp3
 {
     class InputView
     {
+        private string readInput()
+        {
+            String input = Console.ReadLine().ToLower();
+            if (input.Equals("s"))
+            {
+                System.Environment.Exit(1);
+            }
+            return input;
+        }
+
+        public int ReadPlayMove()
+        {
+            String input = readInput();
+            if (char.IsNumber(input[0]) && input[0] <= 5)
+            {
+                return int.Parse(Char.ToString(input[0]));
+            }
+            return -1;
+        }
     }
 }
--- ConsoleApp3/OutputView.cs
+++ ConsoleApp3/OutputView.cs
@@ -8,5 +8,46 @@ namespace ConsoleApp3
 {
     class OutputView
     {
+        public void showTutuorial()
+        {
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("| Welkom bij GoudKoorts                             |");
+            Console.WriteLine("|                                                   |");
+            Console.WriteLine("| betekenis van de symbolen   |  doel van het spel  |");
+            Console.WriteLine("|    ~ : water                |                     |");
+            Console.WriteLine("|  <0> : schip                |  behaal zo veel     |");
+            Console.WriteLine("|  <8> : vol schip            |  mogelijk punten    |");
+            Console.WriteLine("|    K : Kade                 |  door het schip vol |");
+            Console.WriteLine("|    o : wagen                |  weg te sturen      |");
+            Console.WriteLine("|    0 : wagen met inhoud     |                     |");
+            Console.WriteLine("|    # : loods                |                     |");
+            Console.WriteLine("|    ? : baanvak met richting |                     |");
+            Console.WriteLine("|    ?: wissel met richting  |                     |");
+            Console.WriteLine("|    ?: rangeerterrein       |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("|                             |                     |");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("| Controls                                          |");
+            Console.WriteLine("|                                                   |");
+            Console.WriteLine("| <s>      : stop het spel                          |");
+            Console.WriteLine("| <omlaag> : verplaats de truck omlaag              |");
+            Console.WriteLine("| <omhoog> : verplaats de truck omhoog              |");
+            Console.WriteLine("| <links>  : verplaats de truck naar links          |");
+            Console.WriteLine("| <richts> : verplaats de truck naar rechts         |");
+            Console.WriteLine("|                                                   |");
+            Console.WriteLine("| overige besturing wordt getoond tijdens het spel  |");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("");
+        }
     }
 }
